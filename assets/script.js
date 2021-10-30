cb71bb3cf8e3bba070ff303d661ac25a6ec52137
kelerik <vincent.ng39@gmail.com>
2021-10-30 15:35:03 -0400

    slideshow experimentation

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,19 @@
 window.onclick = function () {
    window.location = "/";
 };
+// SLIDESHOW
+var slideIndex = 0;
+
+function showSlides() {
+   var slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {
+      slideIndex = 1;
+   }
+   slides[slideIndex - 1].style.display = "block";
+   setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+showSlides();
