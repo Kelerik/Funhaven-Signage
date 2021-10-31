61f22aa5dc9cac22fbc9b9ba03d7a90f9558af37
kelerik <vincent.ng39@gmail.com>
2021-10-31 14:00:21 -0400

    new slideshow fade effect

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,37 @@
 window.onclick = function () {
     window.location = "/";
 };
-// SLIDESHOW
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
-var slideIndex = 0;
 
-function showSlides() {
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+// SLIDESHOW
+// https://stackoverflow.com/questions/25347946/add-fade-effect-in-slideshow-javascript
+// NOTE: fade effect seems choppy when the images aren't fully loaded yet
+
+// get array of all the elements with this class
+var imgArray = document.getElementsByClassName("slide-img");
+// other slide variables
+var slideIndex = 0,
+    slideDuration = 6000;
+
+function slideShow() {
+    // apply the fadeOut class to make the image fade out using the opacity property
+    document.getElementById("slide-container").className += "fadeOut";
+    // every X milliseconds, change the src attribute to the textContent of the HTML element. this assumes the HTML file path text
+    setTimeout(
+        function () {
+            document.getElementById("slide-container").src =
+                imgArray[slideIndex].textContent;
+            // then remove the fade out class
+            document.getElementById("slide-container").className = "";
+        },
+        // important: set this duration to the same as in the CSS property
+        1000
+    );
+    // iterate
     slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
+    if (slideIndex == imgArray.length) {
+        slideIndex = 0;
     }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 6000); // Change image every 2 seconds
+    // function calls itself to start a loop
+    setTimeout(slideShow, slideDuration);
 }
-showSlides();
+slideShow();
