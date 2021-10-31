0b99b5b6437b6a2531cc0236e046b985b2576773
kelerik <vincent.ng39@gmail.com>
2021-10-31 11:34:49 -0400

    refactor

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,21 @@
 // click anywhere on screen to return to home page
 window.onclick = function () {
-   window.location = "/";
+    window.location = "/";
 };
 // SLIDESHOW
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
 var slideIndex = 0;
 
 function showSlides() {
-   var slides = document.getElementsByClassName("mySlides");
-   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-   }
-   slideIndex++;
-   if (slideIndex > slides.length) {
-      slideIndex = 1;
-   }
-   slides[slideIndex - 1].style.display = "block";
-   setTimeout(showSlides, 6000); // Change image every 2 seconds
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
 showSlides();
