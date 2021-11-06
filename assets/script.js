91c54f3c1e4dca046a103f766bc3825ff857d43e
kelerik <vincent.ng39@gmail.com>
2021-11-06 12:14:59 -0400

    remove console error when no slideshow

--- assets/script.js
+++ assets/script.js
@@ -17,41 +17,46 @@ function shuffleArray(array) {
 // inspired by https://stackoverflow.com/a/25348073
 // NOTE: fade effect seems choppy when the images aren't fully loaded yet
 
-var slider = {
-    // get array of all the <a> elements inside the slideshow element
-    // NOTE: this array contains A LOT of information. we are only using the "href" attribute later below
-    array: document.getElementById("slide-container").getElementsByTagName("a"),
-    index: 0,
-    duration: 6000,
-};
+// don't run any slideshow code if no slideshow on page
+if (document.getElementById("slide-container")) {
+    var slider = {
+        // get array of all the <a> elements inside the slideshow element
+        // NOTE: this array contains A LOT of information. we are only using the "href" attribute later below
+        array: document
+            .getElementById("slide-container")
+            .getElementsByTagName("a"),
+        index: 0,
+        duration: 6000,
+    };
 
-function slideShow() {
-    // apply the fadeOut class to make the image fade out using the opacity property
-    document.getElementById("slide-container").className += "fadeOut";
-    // every X milliseconds, change the "src" attribute of the <img> element to the "href" of the <a> element
-    setTimeout(
-        function () {
-            document
-                // search for the slide element
-                .getElementById("slide-container")
-                // then search for the <img> elements inside it (we are assuming there will only be one, so use the 0th index)
-                .getElementsByTagName("img")[0].src =
-                // then modify the "src" attribute to the "href" of the array
-                slider.array[slider.index].href;
-            // then remove the fade out class
-            document.getElementById("slide-container").className = "";
-        },
-        // important: set this time to the same as in the CSS property
-        1000
-    );
-    // iterate
-    slider.index++;
-    if (slider.index >= slider.array.length) {
-        slider.index = 0;
+    function slideShow() {
+        // apply the fadeOut class to make the image fade out using the opacity property
+        document.getElementById("slide-container").className += "fadeOut";
+        // every X milliseconds, change the "src" attribute of the <img> element to the "href" of the <a> element
+        setTimeout(
+            function () {
+                document
+                    // search for the slide element
+                    .getElementById("slide-container")
+                    // then search for the <img> elements inside it (we are assuming there will only be one, so use the 0th index)
+                    .getElementsByTagName("img")[0].src =
+                    // then modify the "src" attribute to the "href" of the array
+                    slider.array[slider.index].href;
+                // then remove the fade out class
+                document.getElementById("slide-container").className = "";
+            },
+            // important: set this time to the same as in the CSS property
+            1000
+        );
+        // iterate
+        slider.index++;
+        if (slider.index >= slider.array.length) {
+            slider.index = 0;
+        }
+        // function calls itself to start a loop
+        setTimeout(slideShow, slider.duration);
     }
-    // function calls itself to start a loop
-    setTimeout(slideShow, slider.duration);
+    shuffleArray(slider.array);
+    slideShow();
 }
-shuffleArray(slider.array);
-slideShow();
 // ------ SLIDESHOW END ------
